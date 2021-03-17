521e6be66fb18f48d8356778bbd8def092b614b4
Gefy Marcos <gefymarcos@gmail.com>
2021-03-17 19:23:50 -0300

    feat: generic api functions

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import { API_KEY, API_URL } from '@env'
+import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
+
+const request = <T>(config: AxiosRequestConfig): AxiosPromise<T> => {
+  return axios.request<T>({url: `${API_URL}/${config.url}?api-key=${API_KEY}`})
+};
+
+const get = <T>(config: AxiosRequestConfig): AxiosPromise<T> =>
+{
+  return request({ ...config, method: 'GET' });
+}
+
+export { get };
